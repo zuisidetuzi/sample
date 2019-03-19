4a843bfe5229ebdf26745f5b9abfae8b4c9e7c86
vagrant <vagrant@homestead>
2019-03-19 09:10:59 +0000

    123

--- app/Models/User.php
+++ app/Models/User.php
@@ -24,13 +24,24 @@ class User extends Authenticatable
      *
      * @var array
      */
-    protected $hidden = [
-        'password', 'remember_token',
-    ];
+    protected $hidden = ['password', 'remember_token'];
+
 
     public function gravatar($size = '100')
     {
         $hash = md5(strtolower(trim($this->attributes['email'])));
         return "http://www.gravatar.com/avatar/$hash?s=$size";
     }
+
+
+    public static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($user) {
+            $user->activation_token = str_random(30);
+        });
+    }
+
+
 }
