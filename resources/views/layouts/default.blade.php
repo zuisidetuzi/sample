62016e7935579401b833da844ff24124ff9ba4cf
vagrant <vagrant@homestead>
2019-03-19 01:44:57 +0000

    111

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <!--title参数，Sample是默认值-->
+    <title>@yield('title','Sample App') - Laravel 新手入门教程</title>
+</head>
+<body>
+@yield('content')
+</body>
+</html>
