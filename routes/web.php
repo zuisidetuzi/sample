63e1700e1f6b95892a8bde1ec79693e02da2711d
vagrant <vagrant@homestead>
2019-03-19 07:18:37 +0000

    111

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,4 @@ Route::get('/about', 'StaticPagesController@about')->name('about');
 
 
 Route::get('signup', 'UsersController@create')->name('signup');
+Route::resource('users', 'UsersController');
